46813dfb125afed5533b81bbf9f6da55e539ba5e
Samir <mksamir013@gmail.com>
2025-08-31 18:33:08 +0530

    update2

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 // src/lib/config.ts
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL ||
-  (typeof window !== 'undefined' ? 'http://localhost:8000' : 'http://127.0.0.1:8000');
+  (typeof window !== 'undefined' ? 'http://194.164.148.133:8003' : 'http://127.0.0.1:8003');
