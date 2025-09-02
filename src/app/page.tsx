120e806c5fd64f9083c3170e19b20da978809d64
aamir <aamir.khan@anjitait.com>
2025-09-02 11:48:53 +0530

    update  test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,8 @@ export default function LoginPage() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-bold mb-6 text-black">Admin Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-black">Admin Loginsssssss</h2>
+        <h2 className="text-2xl font-bold mb-6 text-black">change mac wdwd</h2>
         
         <input
           suppressHydrationWarning
