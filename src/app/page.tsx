ecd4d084b93011e754e406af721bfecad2b8b456
Samir <mksamir013@gmail.com>
2025-09-02 22:11:11 +0530

    Test auto-deploy webhook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,8 @@ export default function LoginPage() {
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold mb-6 text-black">Admin Loginsssssss</h2>
         <h2 className="text-2xl font-bold mb-6 text-black">change mac wdwd</h2>
+        <h1>Welcome to My App 🚀 (Auto Deploy Test)</h1>
+
         
         <input
           suppressHydrationWarning
